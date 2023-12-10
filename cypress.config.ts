cff1f1355f34733feb672de5af7251602586cef8
Bogdan Dorofeev <bogdan.dorofeev092@gmail.com>
2023-12-10 21:31:37 +0200

    lesson_17_hw HomeWork

--- cypress.config.ts
+++ cypress.config.ts
@@ -16,7 +16,15 @@ export default defineConfig({
   // retries: 1,
   defaultCommandTimeout: 5000,   // default 4000
   requestTimeout: 10000,         // default 5000
-  responseTimeout: 30000,         // default 30000
+  responseTimeout: 30000,        // default 30000
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+
   // watchForFileChanges: false,
   env: {
     password: process.env.PASS
